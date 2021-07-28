454eac7b414f133b16febed742ab86e8f23eeeba
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-07-28 14:00:59 +0530

    Testimonials Added

--- components/Testimonials/Testimonial.js
+++ components/Testimonials/Testimonial.js
@@ -22,14 +22,7 @@ function SlideCard(props){
 export default function Testimonial() {
     const [Index,setIndex]=useState(0)
     
-    // useEffect(()=>{
-    //     if(Index==0){
-    //         for(let i=0;i<4;i++){ 
-    //             const slidecard=document.querySelector('#Card'+i);
-    //             slidecard.setAttribute('style', `transform: translateX(${(Index)*-610}px)`);   
-    //             }
-    //     }
-    // },[Index])
+    const Dummy=()=>{}
 
     const Left=()=>{
         console.log("left clicked")    
@@ -58,20 +51,20 @@ export default function Testimonial() {
         <div className={styles.testimonial}>
             <h2 className={styles.heading}>Testimonials</h2>
             <div className={styles.cardSlider}>
-                <SlideCard imgPath="/assets/feature3.png" name="Hello 123" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card0' />
-                <SlideCard imgPath="/assets/feature3.png" name="Hello 1234" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card1'/>
-                <SlideCard imgPath="/assets/feature3.png" name="Hello 12345" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card2'/>
-                <SlideCard imgPath="/assets/feature3.png" name="Hello 123456" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card3'/>
+                <SlideCard imgPath="/assets/test1.png" name="John Fang" link="wordfaang.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card0' />
+                <SlideCard imgPath="/assets/test2.png" name="Jenny Doe" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card1'/>
+                <SlideCard imgPath="/assets/test3.png" name="Walter Wright" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card2'/>
+                <SlideCard imgPath="/assets/test1.png" name="John Fang" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card3'/>
             </div>
             <div className={styles.last}>
                 <div className={styles.activedots}>
-                    <span className={(Index==0)?styles.activedot:styles.dot}></span>
+                    <span className={(Index<=0)?styles.activedot:styles.dot}></span>
                     <span className={(Index==1)?styles.activedot:styles.dot}></span>
                     <span className={(Index==2)?styles.activedot:styles.dot}></span>
                     <span className={(Index==3)?styles.activedot:styles.dot}></span>
                 </div>
                 <div className={styles.arrows}>
-                    <BsArrowLeft color="#ffffff"  onClick={Left} style={(Index!=0)?{fontSize:'2rem',marginRight:'0.5rem'}:{marginRight:'0.5rem'}}/>
+                    <BsArrowLeft color="#ffffff"  onClick={(Index>0)?Left:Dummy} style={(Index>0)?{fontSize:'2rem',marginRight:'0.5rem'}:{marginRight:'0.5rem'}}/>
                     <BsArrowRight color="#ffffff" onClick={Right} style={(Index!=3)?{fontSize:'2rem'}:{}}/>
                 </div>
             </div>

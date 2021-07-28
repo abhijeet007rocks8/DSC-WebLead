d96f63e51ebbf63ddff7fc121966996f14ceeb77
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-07-28 13:47:05 +0530

    Testimonials Added

--- components/Features/Features.js
+++ components/Features/Features.js
@@ -3,15 +3,15 @@ import styles from './style.module.scss'
 import {BsArrowRight} from 'react-icons/bs'
 
 
-function Card(Data){
+function Card(props){
     return(
         <div className={styles.card}>
             <div className={styles.img}>
-                <img src={Data.imgPath} width="100%" height="auto" alt="card image"/>
+                <img src={props.imgPath} width="100%" height="auto" alt="card image"/>
             </div>
-            <div className={styles.info} style={{backgroundImage:`${Data.bgColor}`}}>
-                <div className={styles.title}>{Data.heading}</div>
-                <div className={styles.subtitle}>{Data.subHeading}</div>
+            <div className={styles.info} style={{backgroundImage:`${props.bgColor}`}}>
+                <div className={styles.title}>{props.heading}</div>
+                <div className={styles.subtitle}>{props.subHeading}</div>
                 <div className={styles.learnMore}>
                     <p className={styles.learn}>Learn More</p>
                     <BsArrowRight/>
@@ -27,14 +27,14 @@ export default function Features() {
             <div className={styles.subheading}>
                 Some of the features and advantages 
                 that we provide for those of you who store 
-                data in this Data Warehouse.
+                props in this props Warehouse.
             </div>
             <div className={styles.grid}>
             
-            <Card imgPath="assets/feature1.png" heading="Search Data" subHeading="Don’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time." bgColor="url('/assets/f1bg.png')"/>
-            <Card imgPath="assets/feature2.png" heading="24 Hour Access" subHeading="Access is given 24 hours a full morning to night and meet again in the morning, giving you comfort when you need data when urgent." bgColor="url('/assets/f2bg.png')"/>
-            <Card imgPath="assets/feature3.png" heading="Print Out" subHeading="Print out service gives you convenience if someday you need print data, just edit it all and just print it." bgColor="url('/assets/f3bg.png')"/>
-            <Card imgPath="assets/feature4.png" heading="Security Code" subHeading="Data Security is one of our best facilities. Allows for your files to be safer. The file can be secured with a code or password that you created, so only you can open the file." bgColor="url('/assets/f4bg.png')"/>
+            <Card imgPath="assets/feature1.png" heading="Search props" subHeading="Don’t worry if your props is very large, the props Warehoue provides a search engine, which is useful for making it easier to find props effectively saving time." bgColor="url('/assets/f1bg.png')"/>
+            <Card imgPath="assets/feature2.png" heading="24 Hour Access" subHeading="Access is given 24 hours a full morning to night and meet again in the morning, giving you comfort when you need props when urgent." bgColor="url('/assets/f2bg.png')"/>
+            <Card imgPath="assets/feature3.png" heading="Print Out" subHeading="Print out service gives you convenience if someday you need print props, just edit it all and just print it." bgColor="url('/assets/f3bg.png')"/>
+            <Card imgPath="assets/feature4.png" heading="Security Code" subHeading="props Security is one of our best facilities. Allows for your files to be safer. The file can be secured with a code or password that you created, so only you can open the file." bgColor="url('/assets/f4bg.png')"/>
 
             </div>
         </div>
--- components/Testimonials/Testimonial.js
+++ components/Testimonials/Testimonial.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import styles from './style.module.scss'
+import {BsArrowRight,BsArrowLeft} from 'react-icons/bs'
+
+
+function SlideCard(props){
+    return(
+        <div className={styles.slidecard} id={props.id}>
+            <div className={styles.profileImg}>
+                <img src={props.imgPath} width="70px" height="70px" alt="profile image" />
+            </div>
+
+            <div className={styles.profileDetails}>
+                <div className={styles.name}>{props.name}</div>
+                <div className={styles.link}><a href={props.link}>{props.link}</a></div>
+                <div className={styles.about}>{props.about}</div>
+            </div>
+        </div>
+    )
+}
 
 export default function Testimonial() {
+    const [Index,setIndex]=useState(0)
+    
+    // useEffect(()=>{
+    //     if(Index==0){
+    //         for(let i=0;i<4;i++){ 
+    //             const slidecard=document.querySelector('#Card'+i);
+    //             slidecard.setAttribute('style', `transform: translateX(${(Index)*-610}px)`);   
+    //             }
+    //     }
+    // },[Index])
+
+    const Left=()=>{
+        console.log("left clicked")    
+        if(Index>0) {
+            for(let i=0;i<4;i++){ 
+            const slidecard=document.querySelector('#Card'+i);
+            slidecard.setAttribute('style', `transform: translateX(${(Index-1)*-610}px)`);   
+            }
+        }
+        setIndex(Index-1)
+    }
+
+
+    const Right=()=>{
+        console.log("right clicked")
+        if(Index<3){
+            if(Index<0) setIndex(0)
+         setIndex((Index+1));
+         for(let i=0;i<4;i++){ 
+            const slidecard=document.querySelector('#Card'+i);
+            slidecard.setAttribute('style', `transform: translateX(${(Index+1)*-610}px)`);
+            }        
+        }
+    }
     return (
-        <div>
-            
+        <div className={styles.testimonial}>
+            <h2 className={styles.heading}>Testimonials</h2>
+            <div className={styles.cardSlider}>
+                <SlideCard imgPath="/assets/feature3.png" name="Hello 123" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card0' />
+                <SlideCard imgPath="/assets/feature3.png" name="Hello 1234" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card1'/>
+                <SlideCard imgPath="/assets/feature3.png" name="Hello 12345" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card2'/>
+                <SlideCard imgPath="/assets/feature3.png" name="Hello 123456" link="www.facebook.com"  about="Suspendisse ultrices at diam lectus nullam. Nisl, sagittis viverra enim erat tortor ultricies massa turpis. Arcu pulvinar aenean nam laoreet nulla." id='Card3'/>
+            </div>
+            <div className={styles.last}>
+                <div className={styles.activedots}>
+                    <span className={(Index==0)?styles.activedot:styles.dot}></span>
+                    <span className={(Index==1)?styles.activedot:styles.dot}></span>
+                    <span className={(Index==2)?styles.activedot:styles.dot}></span>
+                    <span className={(Index==3)?styles.activedot:styles.dot}></span>
+                </div>
+                <div className={styles.arrows}>
+                    <BsArrowLeft color="#ffffff"  onClick={Left} style={(Index!=0)?{fontSize:'2rem',marginRight:'0.5rem'}:{marginRight:'0.5rem'}}/>
+                    <BsArrowRight color="#ffffff" onClick={Right} style={(Index!=3)?{fontSize:'2rem'}:{}}/>
+                </div>
+            </div>
         </div>
     )
 }
